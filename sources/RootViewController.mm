3a8aac0acc998ebde4e406a39ddc227bc7e9cfae
willgon <willgon@foxmail.com>
2025-06-07 17:54:54 +0800

    优化调整

--- sources/RootViewController.mm
+++ sources/RootViewController.mm
@@ -530,7 +530,7 @@ static const CGFloat _gAuthorLabelBottomConstraintConstantRegular = -80.f;
         return;
     }
     // 设置作者标签文本为提示信息
-    [_authorLabel setText:NSLocalizedString(@"Tap that button on the center again,\nto toggle ON/OFF "Dynamic Island" mode.", nil)];
+    [_authorLabel setText:NSLocalizedString(@"Tap that button on the center again,\nto toggle ON/OFF \"Dynamic Island\" mode.", nil)];
 }
 
 - (BOOL)settingHighlightedWithKey:(NSString * _Nonnull)key
@@ -627,11 +627,11 @@ static const CGFloat _gAuthorLabelBottomConstraintConstantRegular = -80.f;
     BOOL isPad = ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad);
     // 如果垂直尺寸类别为紧凑型
     if (verticalClass == UIUserInterfaceSizeClassCompact) {
-        CGFloat topConstant = _gTopButtonConstraintsConstantCompact;
+        // 移除未使用的变量
         [_settingsButton setHidden:YES]; // 隐藏设置按钮
         [_authorLabelBottomConstraint setConstant:_gAuthorLabelBottomConstraintConstantCompact]; // 设置作者标签底部约束
     } else {
-        CGFloat topConstant = isPad ? _gTopButtonConstraintsConstantRegularPad : _gTopButtonConstraintsConstantRegular;
+        // 移除未使用的变量
         [_settingsButton setHidden:NO]; // 显示设置按钮
         [_authorLabelBottomConstraint setConstant:_gAuthorLabelBottomConstraintConstantRegular]; // 设置作者标签底部约束
     }
